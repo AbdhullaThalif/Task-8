3d14927953077adaf6e78d4f15b32551adcf381d
AbdhullaThalif <158301949+AbdhullaThalif@users.noreply.github.com>
2024-02-19 14:03:19 +0530

    Add files via upload

---
+++ Task8_1.java
@@ -0,0 +1,16 @@
+package javaprogramming;
+
+public class Task8_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10,b=40,c=15,d=16;
+		if ((a+b)>=(c+d)){
+			System.out.println("Sum of a and b is greater than the sum of c and d");
+		} else {
+			System.out.println("Sum of a and b is not greater than the sum of c and d");
+		}
+	}
+
+}
